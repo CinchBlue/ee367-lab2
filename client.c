4720f9036f6ae99c9da84b95050d05df20a3a46b
nelsonhp <nelsonhp@hawaii.edu>
2016-02-04 17:37:30 -1000

    Create client.c

---
+++ client.c
@@ -0,0 +1,177 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define SERVERPORT "4950"    // the port users will be connecting to
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+
+int sockfd;
+int socketOpen = 0;
+struct addrinfo hints, *servinfo, *p;
+int rv;
+int numbytes;
+char buf[MAXDATASIZE];
+
+void closeSocket();
+
+int main(int argc, char *argv[]) {
+
+	if (argc != 3) {
+		fprintf(stderr, "usage: talker hostname message\n");
+		exit(1);
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and make a socket
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+				== -1) {
+			perror("talker: socket");
+			continue;
+		} else {
+			socketOpen = 1;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "talker: failed to create socket\n");
+		return 2;
+	} else {
+		socketOpen = 1;
+	}
+
+	if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0, p->ai_addr,
+					p->ai_addrlen)) == -1) {
+				perror("talker: sendto");
+				closeSocket();
+				exit(1);
+				if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+			        perror("recv");
+			        exit(1);
+			    }
+
+
+		//closeSocket();
+	}
+		freeaddrinfo(servinfo);
+
+		//printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+
+		return 0;
+
+}
+
+void closeSocket(){
+	close(sockfd);
+	socketOpen = 0;
+}
+
+/*
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <errno.h>
+ #include <string.h>
+ #include <netdb.h>
+ #include <sys/types.h>
+ #include <netinet/in.h>
+ #include <sys/socket.h>
+
+ #include <arpa/inet.h>
+
+ #define PORT "3490" // the port client will be connecting to
+
+ #define MAXDATASIZE 100 // max number of bytes we can get at once
+
+ // get sockaddr, IPv4 or IPv6:
+ void *get_in_addr(struct sockaddr *sa) {
+ if (sa->sa_family == AF_INET) {
+ return &(((struct sockaddr_in*) sa)->sin_addr);
+ }
+
+ return &(((struct sockaddr_in6*) sa)->sin6_addr);
+ }
+
+ int main(int argc, char *argv[]) {
+ int sockfd, numbytes;
+ char buf[MAXDATASIZE];
+ struct addrinfo hints, *servinfo, *p;
+ int rv;
+ char s[INET6_ADDRSTRLEN];
+
+ if (argc != 2) {
+ fprintf(stderr, "usage: client hostname\n");
+ exit(1);
+ }
+
+ memset(&hints, 0, sizeof hints);
+ hints.ai_family = AF_UNSPEC;
+ hints.ai_socktype = SOCK_STREAM;
+
+ if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+ fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+ return 1;
+ }
+
+ // loop through all the results and connect to the first we can
+ for (p = servinfo; p != NULL; p = p->ai_next) {
+ if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+ == -1) {
+ perror("client: socket");
+ continue;
+ }
+
+ if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+ close(sockfd);
+ perror("client: connect");
+ continue;
+ }
+
+ break;
+ }
+
+ if (p == NULL) {
+ fprintf(stderr, "client: failed to connect\n");
+ return 2;
+ }
+
+ inet_ntop(p->ai_family, get_in_addr((struct sockaddr *) p->ai_addr), s,
+ sizeof s);
+ printf("client: connecting to %s\n", s);
+
+ freeaddrinfo(servinfo); // all done with this structure
+
+ if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
+ perror("recv");
+ exit(1);
+ }
+
+ buf[numbytes] = '\0';
+
+ printf("client: received '%s'\n", buf);
+
+ close(sockfd);
+
+ return 0;
+ }
+
+ */
